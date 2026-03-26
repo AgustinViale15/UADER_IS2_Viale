080c394fc8e81313198306d255ef64964e0c05dc
Agustin <viale151227@gmail.com>
2026-03-26 14:04:57 -0300

    Modificación 1: Si no hay argumento, solicita el número

--- src/factorial/factorial.py
+++ src/factorial/factorial.py
@@ -6,23 +6,34 @@
 #* Creative commons                                                        *
 #*-------------------------------------------------------------------------*
 import sys
-def factorial(num): 
-    if num < 0: 
-        print("Factorial de un número negativo no existe")
-        return 0
-    elif num == 0: 
-        return 1
-        
-    else: 
-        fact = 1
-        while(num > 1): 
-            fact *= num 
-            num -= 1
-        return fact 
+def factorial(n):
+   
+    if n < 0:
+        return None
+    resultado = 1
+    for i in range(1, n + 1):
+        resultado *= i
+    return resultado
 
-if len(sys.argv) == 0:
-   print("Debe informar un número!")
-   sys.exit()
-num=int(sys.argv[1])
-print("Factorial ",num,"! es ", factorial(num)) 
 
+if len(sys.argv) > 1:
+    
+    try:
+        numero = int(sys.argv[1])
+    except ValueError:
+        print("Error: El argumento debe ser un número entero")
+        sys.exit(1)
+else:
+    
+    try:
+        numero = int(input("Ingrese un número para calcular su factorial: "))
+    except ValueError:
+        print("Error: Debe ingresar un número entero")
+        sys.exit(1)
+
+
+if numero < 0:
+    print("Error: El factorial no está definido para números negativos")
+else:
+    resultado = factorial(numero)
+    print(f"El factorial de {numero} es: {resultado}")
